d33b19e3bda19bca61e55621c5b0992b81a11cb8
Yash Soni <120086351+Yash-Soni7744@users.noreply.github.com>
2023-09-13 21:57:54 +0530

    this is it.

---
+++ insertion(atanyposition).cpp
@@ -0,0 +1,50 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+struct node{
+  int data;
+  struct node*next;
+};
+
+struct node*head = NULL;
+
+//displaying the list
+
+void printlist(){
+    struct node*p = head;
+    cout<<"\n[ ";
+    while(p!=NULL){
+        cout<<p->data<<" ";
+        p=p->next;
+    }
+    cout<<"]";
+}
+
+//insertion at beginning
+void insertatbegin(int data){
+    struct node*newnode = new struct node;
+    newnode->data = data;
+    newnode->next = head;
+    head = newnode;
+}
+
+//insertion afternode
+
+void insertafternode(struct node*list,int data){
+    struct node*newnode= new struct node;
+    newnode->data = data;
+    newnode->next=list->next;
+    list->next = newnode;
+}
+int main() {
+    // Write C++ code here
+    insertatbegin(2);
+    insertatbegin(55);
+    insertatbegin(33);
+    insertafternode(head->next,40);
+    insertafternode(head->next,62);
+    cout<<"linked list:\n";
+    printlist();
+    return 0;
+}
