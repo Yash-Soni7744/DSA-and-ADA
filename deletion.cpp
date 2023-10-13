8fa47048b83866519385eb31b1c9cfd2b6cf89b1
Yash Soni <120086351+Yash-Soni7744@users.noreply.github.com>
2023-10-13 15:18:12 +0530

    Update deletion.cpp

--- deletion.cpp
+++ deletion.cpp
@@ -18,7 +18,7 @@ void printlist(){
         cout<<p->data<<" ";
         p=p->next;
     }
-    cout<<"]";
+    cout<<"]\n";
 }
 void insertatbegin(int data){
     struct node*newnode = new struct node;
@@ -26,30 +26,58 @@ void insertatbegin(int data){
     newnode->next = head;
     head = newnode;
 }
-//deletion at end
 
-void deleteatend(){
-    struct node*linkedlist=head;
-    while(linkedlist->next->next!=NULL){
-        linkedlist=linkedlist->next;
+//deletion at beginning
+
+void deletebegin(){
+    if (head != NULL) {
+        struct node*temp = head;
+        head = head->next;
+        delete temp;
     }
+}
+
+//deletion at end
+void deleteatend(){
+    if (head == NULL || head->next == NULL) {
+        // If the list is empty or has only one node, do nothing
+        return;
+    }
+    struct node* linkedlist = head;
+    while (linkedlist->next->next != NULL) {
+        linkedlist = linkedlist->next;
+    }
+    delete linkedlist->next;
     linkedlist->next = NULL;
 }
+
 // delete the node
-void deletenode(int key){
-    struct node*temp = head,*prev;
-    if(temp! = NULL && temp->data == key){
+void deletenode(int key) {
+    struct node* temp = head;
+    struct node* prev = NULL;
+
+    // If the node to be deleted is the head node
+    if (temp != NULL && temp->data == key) {
         head = temp->next;
+        delete temp;
+        return;
     }
-    return;
-}
-// Find key to be deleted
-while (temp!=NULL && temp -> data!=key){
-    prev = temp;
-    temp = temp->next;
-}
-if (temp == NULL){
+
+    // Search for the node to be deleted, keep track of the previous node
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    // If the node with the key is not present in the list
+    if (temp == NULL) {
+        cout << "Node with key " << key << " not found in the list." << endl;
+        return;
+    }
+
+    // Unlink the node from the linked list
     prev->next = temp->next;
+    delete temp;
 }
 
 //programme start
@@ -62,7 +90,8 @@ int main(){
     insertatbegin(52);
     cout<<"linked list \n";
     printlist();
+    deletebegin();
     deleteatend();
-    cout<<"linked list after deletion \n"
+    cout<<"linked list after deletion \n";
     printlist();
 }
