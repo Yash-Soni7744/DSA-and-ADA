b8e556b56394b1f0154dbe11c7a4b0fcd2a7de56
Yash Soni <120086351+Yash-Soni7744@users.noreply.github.com>
2023-11-29 21:01:08 +0530

    Update queue-using-array.cpp

--- queue-using-array.cpp
+++ queue-using-array.cpp
@@ -23,4 +23,49 @@ void Delete() {
         return;
     } else {
         cout << "Element deleted from queue is : " << queue[front] << endl;
-        front++
+        front++;
+    }
+}
+
+void Display() {
+    if (front == -1)
+        cout << "Queue is empty" << endl;
+    else {
+        cout << "Queue elements are : ";
+        for (int i = front; i <= rear; i++)
+            cout << queue[i] << " ";
+        cout << endl;
+    }
+}
+
+int main() {
+    int ch;
+    cout << "1) Insert element to queue" << endl;
+    cout << "2) Delete element from queue" << endl;
+    cout << "3) Display all the elements of queue" << endl;
+    cout << "4) Exit" << endl;
+
+    do {
+        cout << "Enter your choice : " << endl;
+        cin >> ch;
+
+        switch (ch) {
+            case 1:
+                Insert();
+                break;
+            case 2:
+                Delete();
+                break;
+            case 3:
+                Display();
+                break;
+            case 4:
+                cout << "Exit" << endl;
+                break;
+            default:
+                cout << "Invalid choice" << endl;
+        }
+    } while (ch != 4);
+
+    return 0;
+}
