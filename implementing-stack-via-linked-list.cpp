9f418fa1f31ca118419b2bf34ceae74314a3ee5e
Yash Soni <120086351+Yash-Soni7744@users.noreply.github.com>
2023-12-05 10:20:55 +0530

    Update implementing-stack-via-linked-list.cpp

--- implementing-stack-via-linked-list.cpp
+++ implementing-stack-via-linked-list.cpp
@@ -1,73 +1,62 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-struct node {
+struct node{
     int data;
-    struct node* next;
+    struct node*next;
 };
 
-struct node* top = NULL;
+struct node*top = NULL;
 
-void push(int val) {
-    struct node* newnode = new struct node;
-    newnode->data = val;
+void push(int data){
+    struct node *newnode = new struct node;
+    newnode->data = data;
     newnode->next = top;
     top = newnode;
+    cout<<" "<<data<<"is pushed into the stack\n";
 }
 
-void pop() {
-    if (top == NULL) {
-        cout << "Stack Underflow" << endl;
+void pop(){
+    if(top == NULL){
+        cout<<"Stack Underflow";
     }
-    else {
-        cout << "The popped element is: " << top->data << endl;
-        top = top->next;
+    cout<<"The popped element is: "<<top->data<<endl;
+    top = top->next;
+}
+void peek(){
+    if (top == NULL){
+        cout<<"Stack Underflow"<<endl;
     }
+    cout<<"The element at top is: "<<top->data<<endl;
 }
 
-void display() {
-    struct node* ptr;
-    if (top == NULL) {
-        cout << "Stack is Empty" << endl;
+void display(){
+    struct node *ptr;
+    if (top == NULL){
+        cout<<"Stack Underflow"<<endl;
     }
-    else {
-        ptr = top;
-        cout << "Stack Elements: [";
-        while (ptr != NULL) {
-            cout << ptr->data << " ";
-            ptr = ptr->next;
-        }
-        cout <<"]"<<endl;
+    ptr = top;
+    cout<<"[";
+    while(ptr!=NULL){
+        cout<<ptr->data<<" ";
+        ptr=ptr->next;
     }
+    cout<<"]";
+}
+int main(){
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    push(50);
+    
+    pop();
+    pop();
+    peek();
+    display();
 }
 
-int main() {
-    int ch, val;
-    while (true) {
-        cout << "1.) Push an element in stack" << endl;
-        cout << "2.) Pop an element from stack" << endl;
-        cout << "3.) Display stack" << endl;
-        cout << "4.) Exit" << endl;
-        cout << "Enter your choice: ";
-        cin >> ch;
 
-        switch (ch) {
-        case 1:
-            cout << "Enter value to push: ";
-            cin >> val;
-            push(val);
-            break;
-        case 2:
-            pop();
-            break;
-        case 3:
-            display();
-            break;
-        case 4:
-            exit(0);
-        default:
-            cout << "Invalid choice! Please enter a valid option." << endl;
-        }
-    }
-    return 0;
-}
+
+
+
