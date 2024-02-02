2019e80b1ac6bdef1729a4712888f1b8caca774c
Yash Soni <120086351+Yash-Soni7744@users.noreply.github.com>
2024-02-02 12:28:03 +0530

    Create matrix-chain-multiply.cpp

---
+++ matrix-chain-multiply.cpp
@@ -0,0 +1,27 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matrix_no,arr[],rows, columns;
+        
+    std::cout<<"enter the no. of matrices to multiply: ";
+    cin>>matrix_no;
+    
+    for(int i=1; i<matrix_no; i++){
+        cout<<"enter the no. of rows for "<<i<<" matrix";
+        cin>>rows;
+        cout<<"enter the no. of columns for "<<i<<" matrix";
+        cin>>columns;
+        
+        int A[rows][columns];
+        arr.push(A);
+    }
+    
+    for(int i=0; i<end(arr)-begin(arr); i++){
+        
+    }
+    
+    
+    
+    
