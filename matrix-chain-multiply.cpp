1d970d2b5910914bb85b204dba49fd60f6b4dbca
Yash Soni <120086351+Yash-Soni7744@users.noreply.github.com>
2024-02-09 12:25:24 +0530

    Update matrix-chain-multiply.cpp

--- matrix-chain-multiply.cpp
+++ matrix-chain-multiply.cpp
@@ -1,27 +1,53 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
+#include <vector>
+#include <climits>
+
 using namespace std;
 
+void printOptimalParentheses(const vector<vector<int>>& s, int i, int j);
+
+void matrixChainOrder(const vector<int>& dimensions) {
+    int n = dimensions.size() - 1;
+    vector<vector<int>> m(n, vector<int>(n, 0));
+    vector<vector<int>> s(n, vector<int>(n, 0));
+
+    for (int length = 2; length <= n; ++length) {
+        for (int i = 0; i < n - length + 1; ++i) {
+            int j = i + length - 1;
+            m[i][j] = INT_MAX;
+            for (int k = i; k < j; ++k) {
+                int cost = m[i][k] + m[k + 1][j] + dimensions[i] * dimensions[k + 1] * dimensions[j + 1];
+                if (cost < m[i][j]) {
+                    m[i][j] = cost;
+                    s[i][j] = k;
+                }
+            }
+        }
+    }
+
+    // Output the optimal parentheses
+    cout << "Optimal Parentheses: ";
+    printOptimalParentheses(s, 0, n - 1);
+    cout << endl;
+
+    // Output the minimum number of scalar multiplications
+    cout << "Minimum Scalar Multiplications: " << m[0][n - 1] << endl;
+}
+
+void printOptimalParentheses(const vector<vector<int>>& s, int i, int j) {
+    if (i == j) {
+        cout << "M" << i;
+    } else {
+        cout << "(";
+        printOptimalParentheses(s, i, s[i][j]);
+        printOptimalParentheses(s, s[i][j] + 1, j);
+        cout << ")";
+    }
+}
+
 int main() {
-    int matrix_no,arr[],rows, columns;
-        
-    std::cout<<"enter the no. of matrices to multiply: ";
-    cin>>matrix_no;
-    
-    for(int i=1; i<matrix_no; i++){
-        cout<<"enter the no. of rows for "<<i<<" matrix";
-        cin>>rows;
-        cout<<"enter the no. of columns for "<<i<<" matrix";
-        cin>>columns;
-        
-        int A[rows][columns];
-        arr.push(A);
-    }
-    
-    for(int i=0; i<end(arr)-begin(arr); i++){
-        
-    }
-    
-    
-    
-    
+    vector<int> dimensions = {30, 35, 15, 5, 10, 20, 25};
+    matrixChainOrder(dimensions);
+
+    return 0;
+}
